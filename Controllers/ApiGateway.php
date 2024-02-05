d786b84bda5a4a06506e253cc2487f07b278ac49
Jose Alves <alvesjose13@gmail.com>
2024-02-05 23:43:36 +0000

    git hub action

--- Controllers/ApiGateway.php
+++ Controllers/ApiGateway.php
@@ -29,12 +29,16 @@ class ApiGateway
      * @param string $php_auth_user php_auth_user
      * @param string $php_auth_pw php_auth_pw
      */
-    public function handleRequest(string $method, string $path, ?array $data, string $authorization, string $php_auth_user, $php_auth_pw)
-    {
+    public function handleRequest(
+        string $method,
+        string $path,
+        ?array $data,
+        string $authorization,
+        string $php_auth_user,
+        string $php_auth_pw
+    ) {
         $this->status = 200;
-
         if ($path != "api/auth") {
-
             /*$jwt = Encrypt::encryptJwt("a4b728c805a50b7d81115ce5d10a39d8-1-0", "E");
             var_dump($jwt);die();*/
             $jwt = Encrypt::decryptJwt($authorization, "E");
@@ -61,11 +65,17 @@ class ApiGateway
      * @param string $php_auth_user php_auth_user
      * @param string $php_auth_pw php_auth_pw
      */
-    private function processRequest(string $method, string $path, ?array $data, string $authorization, string $php_auth_user, string $php_auth_pw)
-    {
-        if($cache = Memcache::getInstance()->get($path)){
+    private function processRequest(
+        string $method,
+        string $path,
+        ?array $data,
+        string $authorization,
+        string $php_auth_user,
+        string $php_auth_pw
+    ) {
+        if ($cache = Memcache::getInstance()->get($path)) {
             $routes = $cache;
-        }else{
+        } else {
             $routes = Routes::findLast("*", ["route" => $path, "status" => 1]);
             if (!$routes) {
                 $this->status = 404;
@@ -87,15 +97,21 @@ class ApiGateway
      * @param string $php_auth_user php_auth_user
      * @param string $php_auth_pw php_auth_pw
      */
-    private function forwardRequest(string $method, string $url, ?array $data, string $authorization, string $php_auth_user, string $php_auth_pw)
-    {
+    private function forwardRequest(
+        string $method,
+        string $url,
+        ?array $data,
+        string $authorization,
+        string $php_auth_user,
+        string $php_auth_pw
+    ) {
         $client = new Client();
         try {
             $response = $client->request($method, $url, [
                 'headers' => ['Content-Type' => 'application/json', "Authorization" => $authorization, "PHP_AUTH_USER" => $php_auth_user, "PHP_AUTH_PW" => $php_auth_pw],
                 'json' => $data,
             ]);
-            $this->status = $response ? $response->getStatusCode() : '0';
+            $this->status = $response != null ? $response->getStatusCode() : '0';
             return json_decode($response->getBody(), true);
         } catch (\GuzzleHttp\Exception\RequestException $e) {
             $response = $e->getResponse();
